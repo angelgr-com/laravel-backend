bee88b21aec0844e65fe4340970d4a61be6e2f4c
angelgr-com <dev@angelgr.com>
2022-04-17 12:29:57 +0200

    fix: recreate changes in api.php, it had a wrong route and still used Forget and Reset Controllers

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ForgetController;
-use App\Http\Controllers\API\ResetController;
+use App\Http\Controllers\AuthController;
 
 // Laravel Passport Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [AuthController::class, 'userInfo'])->middleware('auth:api');
-Route::post('/forget', [ForgetController::class, 'forget']);
-Route::post('/reset', [ResetController::class, 'reset']);
+Route::post('/forget', [AuthController::class, 'forget']);
+Route::post('/reset', [AuthController::class, 'reset']);
