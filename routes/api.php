d99f1d525e961e6fd675c54e2bb42f6108bd26bf
angelgr-com <dev@angelgr.com>
2022-04-17 02:53:59 +0200

    feat: add user method in api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PassportAuthController;
 use App\Http\Controllers\API\ForgetController;
 use App\Http\Controllers\API\ResetController;
+use App\Http\Controllers\API\UserController;
 
 Route::get('/hi', function () {
     return 'hi!';
@@ -15,4 +16,5 @@ Route::post('/register', [PassportAuthController::class, 'register']);
 Route::post('/login', [PassportAuthController::class, 'login']);
 Route::post('/forget', [ForgetController::class, 'forget']);
 Route::post('/reset', [ResetController::class, 'reset']);
+Route::get('/user', [UserController::class, 'user'])->middleware('auth:api');
 
