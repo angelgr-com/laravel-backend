b48efb3d5d2990d8a8bc2c1b1fb28a4a7c6ae3fd
angelgr-com <dev@angelgr.com>
2022-04-17 13:17:55 +0200

    fix: add new endpoint /reset/{pincode} to avoid 404 error in password reset link

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [AuthController::class, 'userInfo'])->middleware('auth:api');
 Route::post('/forget', [AuthController::class, 'forget']);
 Route::post('/reset', [AuthController::class, 'reset']);
+Route::post('/reset/{pincode}', [AuthController::class, 'reset']);
